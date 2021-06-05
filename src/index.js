8423dcf4c66c0fd9acdfdf1d5f111ac302c4bd9d
ykudnn <ykudnyksb@gmail.com>
2021-06-06 02:52:54 +0900

    display which player has the next turn

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext? 'X' : 'O';
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
@@ -38,7 +38,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: '+(this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
